bd568a707066092a6b4f5ae61f6c42d2a1339a63
A. Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2026-07-18 02:07:44 +0300

    display_name not null

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,5 +4,5 @@ export const usersTable = pgTable("users", {
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  display_name: varchar({ length: 255 }),
+  display_name: varchar({ length: 255 }).notNull(),
 });
